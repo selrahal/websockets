2c9816669731e355b8e975c8007f8b013ffbefa7
Salem Elrahal <salemelrahal@gmail.com>
2015-03-02 16:29:37 -0500

    cleaned up js

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// Declare app level module which depends on filters, and services
 angular.module('webSockets', ['ngRoute']).
   config(['$routeProvider','$httpProvider', function($routeProvider, $httpProvider) {
     $routeProvider.when('/home', {templateUrl: 'app/home.html'});
